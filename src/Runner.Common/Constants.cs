0e0115ad98c0def4ae0785a93ef49f3ad5d840c3
Max Horstmann <maxhorstmann@microsoft.com>
2026-03-04 16:41:28 -0500

    try /tmp

--- src/Runner.Common/Constants.cs
+++ src/Runner.Common/Constants.cs
@@ -249,7 +249,7 @@ namespace GitHub.Runner.Common
         public static class FuseMount
         {
             public static readonly string DriverPath = "/usr/local/bin/codespaces-fuse-driver";
-            public static readonly string BaseCacheDir = "/cache";
+            public static readonly string BaseCacheDir = "/tmp/cache";
             public static readonly string CacheFile = System.IO.Path.Combine(BaseCacheDir, "cache");
             public static readonly string WritableFile = System.IO.Path.Combine(BaseCacheDir, "writable");
             public static readonly string LayersPath = System.IO.Path.Combine(BaseCacheDir, "layers.json");
